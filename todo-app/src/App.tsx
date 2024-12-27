de642beb2b2b24392682257d3b39b123093f4c6f
oguzhantimur4444 <oguzhantimur4444@gmail.com>
2024-12-27 11:22:08 +0300

    App auth forms initialized

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,13 +1,16 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import ProtectedRoute from "./ProtectedRoute";
+import Login from "./Login";
+import Register from "./Register";
 import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Home />} />
-        <Route path="/register" element={<Home />} />
+        <Route path="/" element={<ProtectedRoute children={<Home/>} ></ProtectedRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
