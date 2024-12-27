f7d525130b029c0c7278d96a01f01a6b39b40d9c
oguzhantimur4444 <oguzhantimur4444@gmail.com>
2024-12-27 13:29:51 +0300

    HotFix

--- todo-api/src/main/java/com/oguzhantimur4444/todo_api/model/User.java
+++ todo-api/src/main/java/com/oguzhantimur4444/todo_api/model/User.java
@@ -1,11 +1,15 @@
 package com.oguzhantimur4444.todo_api.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
