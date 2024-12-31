fa5ed01e39cae2bd16470837525c204e5d2f7266
oguzhantimur4444 <oguzhantimur4444@gmail.com>
2024-12-31 12:38:31 +0300

    asd

--- todo-app/src/pages/Home.tsx
+++ todo-app/src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from "react-router-dom"
+import { logout } from "../Auth"
+
 export default function Home() {
+  const navigate = useNavigate();
+
+
+  const handleEvent = () => {
+    logout()
+    navigate('/login')
+    
+  }
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <button onClick={handleEvent} type="submit">LogOut</button>
+    </div>
   )
 }
